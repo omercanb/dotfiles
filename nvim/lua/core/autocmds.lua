d6c2cd87287e53d61584ddaf6bcf5e6a44788b85
omercanb <omercanbaykara@gmail.com>
2026-02-01 22:59:12 +0300

    Added osc

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -9,5 +9,21 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.hl.on_yank()
+
+    -- OSC 52 to enable yanks in ssh to get copied to system clipboard
+    local text = vim.fn.getreg '"'
+    text = text:gsub('\\', '\\\\')
+    text = text:gsub("'", "'\\''")
+
+    local encoded = vim.fn.system("echo -n '" .. text .. "' | base64 | tr -d '\\n'")
+
+    local osc
+    if vim.env.TMUX and vim.env.TMUX ~= '' then
+      osc = string.format('\x1bPtmux;\x1b\x1b]52;;%s\x1b\x1b\\\\\x1b\\', encoded)
+    else
+      osc = string.format('\x1b]52;;%s\x1b\\', encoded)
+    end
+
+    vim.fn.system('echo -en "' .. osc .. '" > /dev/tty')
   end,
 })
