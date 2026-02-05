bf077f1dfe89952b5f883eb26bb679fb796e68b9
omercanb <omercanbaykara@gmail.com>
2026-02-05 18:30:33 +0300

    Update oil.lua

--- nvim/lua/custom/plugins/oil.lua
+++ nvim/lua/custom/plugins/oil.lua
@@ -11,10 +11,12 @@ return {
       --      ['h'] = { 'actions.parent', mode = 'n' },
       --      ['l'] = { 'actions.select', mode = 'n' },
     },
+    delete_to_trash = true,
   },
   -- Optional dependencies
   dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  --
   lazy = false,
 }
